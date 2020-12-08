1d2318700e618bbffc58cc1356b1ee54ad758959
khalilbenyahyaten <2300@holbertonschool.com>
2020-12-08 17:29:21 +0000

    remove char at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    ch = ''
+    if n > len(str) or n < 0:
+        return str
+    for i in str:
+        if i != str[n]:
+            ch += i
+    return ch
