571ef5d801b3a57a58ab965e714850e0de085990
khalilbenyahyaten <2300@holbertonschool.com>
2021-01-11 13:19:16 +0000

    area and perimeter of a rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -45,4 +45,7 @@ class Rectangle:
 
     def perimeter(self):
         """perimeter of a rectangle"""
-        return (self.__width + self.__height) * 2
+        if self.__width = 0 or self.__height = 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
