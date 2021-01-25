e3130f42f01e8b35798c2e693cf6555532c773eb
khalilbenyahyaten <2300@holbertonschool.com>
2021-01-25 11:44:43 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" read file """
 def read_file(filename=""):
+    """ read utf-8 file """
     with open(filename, "r", encoding="utf-8") as a:
         print(a.read(), end="")
