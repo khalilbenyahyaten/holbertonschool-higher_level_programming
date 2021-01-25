ffd6a17a80d6590cd44a9c97d5d00a71b501ab25
khalilbenyahyaten <2300@holbertonschool.com>
2021-01-25 11:22:02 +0000

    rm

--- 0x07-python-test_driven_developement/0-add_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" add integers or floats """
-def add_integer(a, b=98):
-    if type(a) not in [int, float]:
-        raise TypeError('a must be an integer')
-    elif type(b) not in [int, float]:
-        raise TypeError('b must be an integer')
-    else:
-        return int(a) + int(b)
