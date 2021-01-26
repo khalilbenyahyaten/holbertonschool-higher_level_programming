420a02219bae770ad6c6807cdcb367c184c03044
khalilbenyahyaten <2300@holbertonschool.com>
2021-01-26 10:25:25 +0000

    print

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" full rectangle """
+BaseGeometry = __import__("8-rectangle").Rectangle
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class """
+
+    def area(self):
+        """ area implementation """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """ print rectangle description """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
