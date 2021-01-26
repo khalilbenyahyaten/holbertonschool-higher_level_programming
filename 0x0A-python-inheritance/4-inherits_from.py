4382a2e12f1daf384a7ee997b7b7ee9d47ec6827
khalilbenyahyaten <2300@holbertonschool.com>
2021-01-26 09:31:58 +0000

    instance of class inheritence

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """checks if an object is an instance of a class that
    inherited from the specified class"""
+
+
 def inherits_from(obj, a_class):
     """ instance of a class """
     if type(obj) != a_class:
