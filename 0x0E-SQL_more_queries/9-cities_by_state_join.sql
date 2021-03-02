62e91465995f70d0a53fd0ef488633d989bcbb56
khalilbenyahyaten <2300@holbertonschool.com>
2021-03-02 19:34:06 +0000

    join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all cities in database
 
 SELECT cities.id, cities.name, states.name FROM cities;
-INNER JOIN states ON cities.state.id = states.id;
+INNER JOIN states ON cities.state_id = states.id;
 ORDER BY cities.id;
