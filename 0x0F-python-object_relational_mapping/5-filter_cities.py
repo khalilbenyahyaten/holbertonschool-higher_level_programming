4ba543222b182fd88be492b60ddb38cb20e2331f
khalilbenyahyaten <2300@holbertonschool.com>
2021-06-16 18:58:34 +0000

    filter cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+filter cities
+"""
+import MySQLdb
+import sys
+
+
+def print_all_cites():
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=sys.argv[1],
+                               passwd=sys.argv[2],
+                               db=sys.argv[3],
+                               charset="utf8"
+                               )
+    cur = database.cursor()
+    cur.execute("SELECT cities.name FROM cities JOIN states ON \
+    cities.state_id=states.id WHERE states.name \
+    LIKE %s ORDER BY cities.id", (sys.argv[4],))
+    rows = cur.fetchall()
+    print(", ".join(city[0] for city in rows))
+    cur.close()
+    database.close()
+
+
+if __name__ == "__main__":
+    print_all_cites()
+    
