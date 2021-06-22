ebf2104b3793b0e8be4ce820cc12e17ceb8646f9
khalilbenyahyaten <2300@holbertonschool.com>
2021-06-22 06:04:33 +0000

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""error code"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    request = requests.get(url)
+    if request.status_code > 400:
+        print('Error code: {}'.format(request.status_code))
+    else:
+        print(request.text)
