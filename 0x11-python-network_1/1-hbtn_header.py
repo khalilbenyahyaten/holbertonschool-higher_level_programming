eee5507096a3dff92cecba9593c4747821f68bef
khalilbenyahyaten <2300@holbertonschool.com>
2021-06-22 05:38:46 +0000

    hbtn header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+hbtn header
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
+        
