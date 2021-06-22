7fbd128cc1d48e7692c1f7731aef6d04d875955a
khalilbenyahyaten <2300@holbertonschool.com>
2021-06-22 05:11:15 +0000

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-""" peak """
+""" peak"""
 def find_peak(list_of_integers):
-    if (not list_of_integers):
+    if list_of_integers == []:
         return None
-    elif (len(list_of_integers) == 1):
+    s = len(list_of_integers)
+    if s == 1:
         return list_of_integers[0]
+    elif s == 2:
+        return max(list_of_integers)
+    half = int(s / 2)
+    p = list_of_integers[half]
+    if p > list_of_integers[half + 1] and p > list_of_integers[half - 1]:
+        return p
+    elif p < list_of_integers[half + 1]:
+        return find_peak(list_of_integers[half + 1:])
     else:
-        i = list_of_integers[0]
-        for j in range(1, len(list_of_integers) - 1):
-            if (list_of_integers[j] >= list_of_integers[j - 1] and list_of_integers[j] >= list_of_integers[j+1]):
-                i = list_of_integers[j]
-                j += 1
-        return i
+        return find_peak(list_of_integers[:half])
         
